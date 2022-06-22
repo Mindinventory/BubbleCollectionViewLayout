68d8a10ce32714e850bd5f77023c7dd333b20a5f
Parth Gohel <parth.gohel@mindinventory.com>
2022-06-22 18:40:02 +0530

    - update design - configure cell with image - handle selection

--- SwiftDemo/SwiftDemo/Class/MIBubbleCollectionViewCell.swift
+++ SwiftDemo/SwiftDemo/Class/MIBubbleCollectionViewCell.swift
@@ -9,14 +9,26 @@
 import UIKit
 
 class MIBubbleCollectionViewCell: UICollectionViewCell {
+
     @IBOutlet var lblTitle:UILabel!
-    
+    @IBOutlet weak var itemImageView: RoundImageView!
+
     override func awakeFromNib() {
         super.awakeFromNib()
+        self.layer.borderWidth = 1
     }
     
     override func layoutSubviews() {
         super.layoutSubviews()
         self.layer.cornerRadius = self.frame.size.height / 2
+    }
+    
+    func configure(with item: Item) {
+
+        lblTitle.text = item.name
+        lblTitle.textColor = item.isSelected ? .white : .black
+        itemImageView.image = item.image
+        layer.borderColor = item.color.cgColor
+        backgroundColor = item.color.withAlphaComponent(item.isSelected ? 1: 0.2)
     }
 }
--- SwiftDemo/SwiftDemo/Class/MIMainViewController.swift
+++ SwiftDemo/SwiftDemo/Class/MIMainViewController.swift
@@ -12,16 +12,12 @@ class MIMainViewController: UIViewController, UICollectionViewDataSource, UIColl
     
     @IBOutlet private var collVData: UICollectionView!
     
-    private let kItemPadding = 15
-    private var arrData = [String]()
+    private let kItemPadding = 10
+    private var arrData: [Item] = Item.mockItems
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        //...
-        arrData = ["None", "Glazed", "Sugar", "Powdered Sugar", "Chocolate with Sprinkles", "Chocolate", "Maple", "Regular", "Chocolate", "Blueberry", "Devil's Food", "None", "Glazed", "Sugar", "Powdered Sugar", "Chocolate with Sprinkles", "Chocolate", "Maple", "None", "Glazed", "Sugar", "Powdered Sugar", "Chocolate with Sprinkles", "Chocolate", "Maple", "Regular", "Chocolate", "Blueberry", "Devil's Food", "None", "Glazed", "Sugar", "Powdered Sugar", "Chocolate with Sprinkles", "Chocolate", "Maple", "None", "Glazed", "Sugar", "Powdered Sugar", "Chocolate with Sprinkles", "Chocolate", "Maple", "Regular", "Chocolate", "Blueberry", "Devil's Food", "None", "Glazed", "Sugar", "Powdered Sugar", "Chocolate with Sprinkles", "Chocolate", "Maple", "None", "Glazed", "Sugar", "Powdered Sugar", "Chocolate with Sprinkles", "Chocolate", "Maple", "Regular", "Chocolate", "Blueberry", "Devil's Food", "None", "Glazed", "Sugar", "Powdered Sugar", "Chocolate with Sprinkles", "Chocolate", "Maple", "None", "Glazed", "Sugar", "Powdered Sugar", "Chocolate with Sprinkles", "Chocolate", "Maple", "Regular", "Chocolate", "Blueberry", "Devil's Food", "None", "Glazed", "Sugar", "Powdered Sugar", "Chocolate with Sprinkles", "Chocolate", "Maple", "None", "Glazed", "Sugar", "Powdered Sugar", "Chocolate with Sprinkles", "Chocolate", "Maple", "Regular", "Chocolate", "Blueberry", "Devil's Food", "None", "Glazed", "Sugar", "Powdered Sugar", "Chocolate with Sprinkles", "Chocolate", "Maple", "None", "Glazed", "Sugar", "Powdered Sugar", "Chocolate with Sprinkles", "Chocolate", "Maple", "Regular", "Chocolate", "Blueberry", "Devil's Food", "None", "Glazed", "Sugar", "Powdered Sugar", "Chocolate with Sprinkles", "Chocolate", "Maple", "None", "Glazed", "Sugar", "Powdered Sugar", "Chocolate with Sprinkles", "Chocolate", "Maple", "Regular", "Chocolate", "Blueberry", "Devil's Food", "None", "Glazed", "Sugar", "Powdered Sugar", "Chocolate with Sprinkles", "Chocolate", "Maple"]
-        
-        
+
         //...UICollectionView setup
         let bubbleLayout = MICollectionViewBubbleLayout()
         bubbleLayout.minimumLineSpacing = 6.0
@@ -42,11 +38,15 @@ class MIMainViewController: UIViewController, UICollectionViewDataSource, UIColl
         let indentifier = "MIBubbleCollectionViewCell"
         
         if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: indentifier, for: indexPath) as? MIBubbleCollectionViewCell {
-            
-            cell.lblTitle.text = arrData[indexPath.row]
+            cell.configure(with: arrData[indexPath.row])
             return cell
         }
         return UICollectionViewCell()
+    }
+
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        arrData[indexPath.row].isSelected.toggle()
+        collectionView.reloadData()
     }
     
     
@@ -55,11 +55,11 @@ class MIMainViewController: UIViewController, UICollectionViewDataSource, UIColl
     
     func collectionView(_ collectionView:UICollectionView, itemSizeAt indexPath:NSIndexPath) -> CGSize {
         
-        let title = arrData[indexPath.row]
+        let title = arrData[indexPath.row].name
         var size = title.size(withAttributes: [NSAttributedString.Key.font: UIFont(name: "HelveticaNeue-Bold", size: 15)!])
-        size.width = CGFloat(ceilf(Float(size.width + CGFloat(kItemPadding * 2))))
-        size.height = 24
-        
+        size.width = CGFloat(ceilf(Float(size.width + 28 + CGFloat(kItemPadding * 2))))
+        size.height = 40
+
         //...Checking if item width is greater than collection view width then set item width == collection view width.
         if size.width > collectionView.frame.size.width {
             size.width = collectionView.frame.size.width
